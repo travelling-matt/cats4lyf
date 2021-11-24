2c950773be01ea0f67b947b6c6ebc63c033b4f1b
Matt Burton <mattwburton@gmail.com>
2021-11-24 17:36:19 +0000

    faker as props

--- src/Api.js
+++ src/Api.js
@@ -6,17 +6,40 @@ import './Api.css';
 const App = () => {
 
   const [catsInfo, setCatsInfo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(
+    {
+      error:false,
+      message: ""
+    }
+  );
+//   const [catAndName, setCatAndName] = useState(
+//       {
+//           url: "",
+//           name: "",
+//           genre: "",
+//           price: ""
+//       }
+//   );
+  const [basketAdd, setBasketAdd] = useState("")
 
   const handler = async () => {
-      
+    try {
+        setLoading(true);
         const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=12") 
         console.log("http response", response)
+        if(response.status !== 200){
+            throw new Error("oops http error");
+        }
         const data = await response.json();
         console.log("API info all", data);
         console.log("API info array 0", data[0]);
         setCatsInfo(data);
         console.log(catsInfo);
-   
+        setLoading(false);
+    } catch(err) {
+    setError({ error:true, message: error.message})
+    }
 };
 
 useEffect(() => {
@@ -27,13 +50,19 @@ if(!catsInfo){
     return <p>loading...</p>
 }
 
+if(error.error){
+    return <h1>{error.message}</h1>
+}
+
 const Cat = (props) => {
     return (
         <div>
             <p><img src={props.pic} className="imgCat rotate" /></p>
-            <p>Hi, my name is {faker.name.firstName()}. I love {faker.music.genre()}</p>
-            <p>£{faker.finance.amount()}</p>
-            <button>please ADOPT ME</button>
+            <p>Hi, my name is {props.name}. I love {props.music}</p>
+            <p>£{props.amount}</p>
+            <p>my name is: {props.name}</p>
+            setCatPrices.push (name, index, faker.finance.amount)
+            <button onClick={() => setBasketAdd(props.name, props.music, props.amount)}>please ADOPT ME</button>
         </div>
     )
 }
@@ -41,7 +70,7 @@ const Cat = (props) => {
 return (
     <div>
         {catsInfo.map((cat, index) => {
-            return <Cat key={index} pic={cat.url} />
+            return <Cat key={index} pic={cat.url} name={faker.name.firstName()} music={faker.music.genre()} amount={faker.finance.amount()}/>
             })}
     </div>
 )
