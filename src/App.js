3100ba21bff99f85870cd7316539b88882565352
Alex Bailey <baileymk@hotmail.com>
2021-11-24 13:11:27 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const Home = () => {
+  return <Cats />
 }
-
-export default App;
+const Cats = (props) => {
+  return (
+    <div className="cats">
+      <img src="" alt="" />
+      <h1>{props.name}</h1>
+      <button onClick=>Add</button>
+    </div>
+  )
+}
